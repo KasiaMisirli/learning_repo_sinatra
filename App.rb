e784f6b60f4717e79d2b8bb15bec20e93c6c50ec
Kasia Misiri <kasia.misirli@travelport.com>
2019-01-24 16:44:34 +1100

    add PUT request

--- App.rb
+++ App.rb
@@ -5,18 +5,20 @@ require 'pry'
 require 'json'
 
 # JSON and DATABASE are constants
-before do
-  response.headers["Access-Control-Allow-Methods"] = "GET", "POST", "DELETE", "OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-  response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-  content_type :json
-end
+rating_questions = []
 
+before do
+  response.headers["Access-Control-Allow-Methods"] = "GET", "POST", "PUT", "DELETE", "OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization", "Content-Type", "Accept", "X-User-Email", "X-Auth-Token"
+  response.headers['Access-Control-Allow-Origin'] = "*"
+  content_type :json
+  rating_questions = JSON.parse(File.read('db.json'))['ratingQuestions']
+
+end
 options "*" do
   200
 end
 
-rating_questions = JSON.parse(File.read('db.json'))['ratingQuestions']
 
 get '/ratingQuestions' do
   rating_questions.to_json
@@ -35,7 +37,6 @@ post '/ratingQuestions' do
     "id": rating_questions.any? ? rating_questions.last["id"]+1 : 1
     
   }
-  binding.pry
   updated_data = rating_questions.push(question)
   File.open("db.json", 'w') do |file|
     file.write(JSON.pretty_generate({ratingQuestions: updated_data}) )
@@ -45,10 +46,21 @@ end
 
 delete '/ratingQuestions/:id' do
   this_id = params[:id]
-  data = rating_questions.each_with_index { |q, i| rating_questions.delete_at(i) if q["id"] == this_id.to_i }
+  rating_questions.each_with_index { |q, i| rating_questions.delete_at(i) if q["id"] == this_id.to_i }
   File.open("db.json", 'w') do |file|
-    file.write(JSON.pretty_generate({ratingQuestions: data}) )
+    file.write(JSON.pretty_generate({ratingQuestions: rating_questions}) )
   end
+  response.status = 200
+  response
 end
 
-
+put '/ratingQuestions/:id' do
+  this_id = params[:id]
+  json_params = JSON.parse(request.body.read)
+  rating_questions.each_with_index { |q,i| q["title"] = json_params["title"] if q["id"] == this_id.to_i}
+  File.open("db.json", 'w') do |file|
+    file.write(JSON.pretty_generate({ratingQuestions: rating_questions}) )
+  end
+  response.status = 200
+  response
+end
